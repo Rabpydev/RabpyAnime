809d64d8b560b1074cba5bce8ed41c8e6cc32a82
Rabpydev <164738118+Rabpydev@users.noreply.github.com>
2024-04-06 08:57:05 +0700

    Create register form
    Register page and function with Django default

--- RabpyWebApp/App_users/views.py
+++ RabpyWebApp/App_users/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import login
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from App_users.forms import RegisterForm
 
 # Create your views here.
+
+def register(request: HttpRequest):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+    else:
+        form = RegisterForm()
+
+    context = {"form": form}
+    return render(request, "user/register.html", context)
